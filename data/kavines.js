b00b0505b7fb14ac4037ac17f5153d9579802c53
naglis mockevicius <48673743+naglismoc@users.noreply.github.com>
2021-06-12 07:59:06 +0300

    first commit

---
+++ data/kavines.js
@@ -0,0 +1,86 @@
+data = [{
+        kavine: 'cili kaunas',
+        adresas: 'juozapaviciaus pr, Kaunas',
+        menu: [{
+                pica: 10.5,
+                vegetariska: ne,
+            },
+            {
+                ledai: 2.5,
+                vegetariska: taip,
+            },
+            {
+                sriuba: 1.2,
+                vegetariska: ne,
+            },
+        ]
+    },
+    {
+        kavine: 'cili santaka',
+        adresas: 'gertrudos g, Kaunas',
+        menu: [{
+                suktinukai: 6.5,
+                vegetariska: ne,
+            },
+            {
+                sonkauliukai: 12.2,
+                vegetariska: ne,
+            },
+            {
+                falafeliai: 4.2,
+                vegetariska: taip,
+            },
+        ]
+    },
+    {
+        kavine: 'cili senamiestis',
+        adresas: 'Mindaugo g, Vilnius',
+        menu: [{
+                suktinukai: 6.5,
+                vegetariska: ne,
+            },
+            {
+                sonkauliukai: 12.2,
+                vegetariska: ne,
+            },
+            {
+                falafeliai: 4.2,
+                vegetariska: taip,
+            },
+        ]
+    },
+    {
+        kavine: 'cili miskas',
+        adresas: 'gertrudos g, Jonava',
+        menu: [{
+                vistienos_kepsnys: 6.5,
+                vegetariska: ne,
+            },
+            {
+                cili_troskinys: 4.2,
+                vegetariska: ne,
+            },
+            {
+                falafeliai: 4.2,
+                vegetariska: taip,
+            },
+        ]
+    },
+    {
+        kavine: 'cili niekam nežinoma',
+        adresas: 'Vaišganto g, kaunas',
+        menu: [{
+                kebabai: 6.5,
+                vegetariska: ne,
+            },
+            {
+                sonkauliukai: 12.2,
+                vegetariska: ne,
+            },
+            {
+                free_bulvytes: 4.2,
+                vegetariska: taip,
+            },
+        ]
+    },
+]
---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+/* 
+Kiekviena užduotis turi būti aprašyta kaip atskira funkcija
+kuriai yra paduodami duomenys. Aprašykite visas funkcijas žemiau. 
+Visas funkcijas iškvieskite vieną po kitos, žemiau šio komentaro.
+
+Taip pat parašykite funkciją kuri: isikviesdama save pačią atspausdina skaičius nuo 1 iki 10
+
+*/
